3668e72b04287835a66a3112f3d630dd4683b4cd
Jason <nj.ho@hotmail.com>
2019-02-26 08:50:45 -0700

    First Commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections;
+
+using Microsoft.AspNetCore.Mvc;
+using Moneris;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Console.WriteLine("Ballsack 1");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+
+        public class MyModel
+        {
+            public string Key { get; set; }
+            public int creditCard { get; set; }
+            public int expiry { get; set; }
+            public string name { get; }
+
+        }
+
+
+        // Hey this is the Vault Endpoint
+        // POST api/values
+        // WHY DID I HAVE TO CREATE A MODEL?
+        // https://stackoverflow.com/questions/40853188/frombody-string-parameter-is-giving-null
+        [HttpPost]
+        public string Post([FromBody] MyModel value)
+        {
+            Console.WriteLine(value.Key);
+
+            string store_id = "store5";
+            string api_token = "yesguy";
+            string pan = "4242424242424242";
+            string expdate = "1912";
+            string phone = "0000000000";
+            string email = "bob@smith.com";
+            string note = "my note";
+            string cust_id = "customer1";
+            string crypt_type = "7";
+            string processing_country_code = "CA";
+            bool status_check = false;
+
+            AvsInfo avsCheck = new AvsInfo();
+            avsCheck.SetAvsStreetNumber("212");
+            avsCheck.SetAvsStreetName("Payton Street");
+            avsCheck.SetAvsZipCode("M1M1M1");
+
+            ResAddCC resaddcc = new ResAddCC();
+            resaddcc.SetPan(pan);
+            resaddcc.SetExpDate(expdate);
+            resaddcc.SetCryptType(crypt_type);
+            resaddcc.SetCustId(cust_id);
+            resaddcc.SetPhone(phone);
+            resaddcc.SetEmail(email);
+            resaddcc.SetNote(note);
+            resaddcc.SetAvsInfo(avsCheck);
+            resaddcc.SetGetCardType("true");
+            //resaddcc.SetDataKeyFormat("0"); //1=F6L4 w/ Length preserve, 2=F6L4 w/o Length preserve
+
+            HttpsPostRequest mpgReq = new HttpsPostRequest();
+            mpgReq.SetProcCountryCode(processing_country_code);
+            mpgReq.SetTestMode(true); //false or comment out this line for production transactions
+            mpgReq.SetStoreId(store_id);
+            mpgReq.SetApiToken(api_token);
+            mpgReq.SetTransaction(resaddcc);
+            mpgReq.SetStatusCheck(status_check);
+            mpgReq.Send();
+
+            try
+            {
+                Receipt receipt = mpgReq.GetReceipt();
+                string dataKey = receipt.GetDataKey();
+                Console.WriteLine(dataKey);
+
+                return dataKey;
+
+                Console.WriteLine("DataKey = " + receipt.GetDataKey());
+                Console.WriteLine("ResponseCode = " + receipt.GetResponseCode());
+                Console.WriteLine("Message = " + receipt.GetMessage());
+                Console.WriteLine("TransDate = " + receipt.GetTransDate());
+                Console.WriteLine("TransTime = " + receipt.GetTransTime());
+                Console.WriteLine("Complete = " + receipt.GetComplete());
+                Console.WriteLine("TimedOut = " + receipt.GetTimedOut());
+                Console.WriteLine("ResSuccess = " + receipt.GetResSuccess());
+                Console.WriteLine("PaymentType = " + receipt.GetPaymentType());
+                Console.WriteLine("Cust ID = " + receipt.GetResDataCustId());
+                Console.WriteLine("Phone = " + receipt.GetResDataPhone());
+                Console.WriteLine("Email = " + receipt.GetResDataEmail());
+                Console.WriteLine("Note = " + receipt.GetResDataNote());
+                Console.WriteLine("MaskedPan = " + receipt.GetResDataMaskedPan());
+                Console.WriteLine("Exp Date = " + receipt.GetResDataExpdate());
+                Console.WriteLine("Crypt Type = " + receipt.GetResDataCryptType());
+                Console.WriteLine("Avs Street Number = " + receipt.GetResDataAvsStreetNumber());
+                Console.WriteLine("Avs Street Name = " + receipt.GetResDataAvsStreetName());
+                Console.WriteLine("Avs Zipcode = " + receipt.GetResDataAvsZipcode());
+                Console.ReadLine();
+
+              
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "It didn't work";
+
+            }
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
